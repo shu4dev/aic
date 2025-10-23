c1d894e2e1537d8a30bf8c9558b0c6cd235d1ac9
Carlos Agüero <cen.aguero@gmail.com>
2025-10-23 03:48:47 +0200

    Add aic_gazebo package (#6)
    * Add aic_gazebo package
    
    Signed-off-by: Carlos Agüero <caguero@osrfoundation.org>
    
    * Tweak
    
    Signed-off-by: Carlos Agüero <caguero@osrfoundation.org>
    
    * Tweak hooks
    
    Signed-off-by: Carlos Agüero <caguero@osrfoundation.org>
    
    ---------
    
    Signed-off-by: Carlos Agüero <caguero@osrfoundation.org>

---
+++ aic_gazebo/include/aic_gazebo/ScoringPlugin.hh
@@ -0,0 +1,59 @@
+
+/*
+ * Copyright (C) 2025 Intrinsic Innovation LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef AIC_GAZEBO__SCORING_PLUGIN_HH_
+#define AIC_GAZEBO__SCORING_PLUGIN_HH_
+
+#include <gz/sim/EventManager.hh>
+#include <gz/sim/System.hh>
+
+namespace aic_gazebo
+{
+  // The main AIC scoring plugin.
+  class ScoringPlugin:
+    public gz::sim::System,
+    public gz::sim::ISystemConfigure,
+    public gz::sim::ISystemPreUpdate,
+    public gz::sim::ISystemUpdate,
+    public gz::sim::ISystemPostUpdate,
+    public gz::sim::ISystemReset
+  {
+    // Documentation inherited
+    public: void Configure(const gz::sim::Entity &_entity,
+                           const std::shared_ptr<const sdf::Element> &_element,
+                           gz::sim::EntityComponentManager &_ecm,
+                           gz::sim::EventManager &_eventManager) override;
+
+    // Documentation inherited
+    public: void PreUpdate(const gz::sim::UpdateInfo &_info,
+                           gz::sim::EntityComponentManager &_ecm) override;
+
+    // Documentation inherited
+    public: void Update(const gz::sim::UpdateInfo &_info,
+                        gz::sim::EntityComponentManager &_ecm) override;
+
+    // Documentation inherited
+    public: void PostUpdate(const gz::sim::UpdateInfo &_info,
+                          const gz::sim::EntityComponentManager &_ecm) override;
+
+    // Documentation inherited
+    public: void Reset(const gz::sim::UpdateInfo &_info,
+                       gz::sim::EntityComponentManager &_ecm) override;
+  };
+}
+#endif
---
+++ aic_gazebo/src/ScoringPlugin.cc
@@ -0,0 +1,69 @@
+
+/*
+ * Copyright (C) 2025 Intrinsic Innovation LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <gz/common/Console.hh>
+#include <gz/plugin/Register.hh>
+#include "aic_gazebo/ScoringPlugin.hh"
+
+GZ_ADD_PLUGIN(
+    aic_gazebo::ScoringPlugin,
+    gz::sim::System,
+    aic_gazebo::ScoringPlugin::ISystemConfigure,
+    aic_gazebo::ScoringPlugin::ISystemPreUpdate,
+    aic_gazebo::ScoringPlugin::ISystemUpdate,
+    aic_gazebo::ScoringPlugin::ISystemPostUpdate,
+    aic_gazebo::ScoringPlugin::ISystemReset
+)
+
+namespace aic_gazebo 
+{
+//////////////////////////////////////////////////
+void ScoringPlugin::Configure(const gz::sim::Entity &_entity,
+                        const std::shared_ptr<const sdf::Element> &/*_element*/,
+                        gz::sim::EntityComponentManager &/*_ecm*/,
+                        gz::sim::EventManager &/*_eventManager*/)
+{
+  gzdbg << "aic_gazebo::ScoringPlugin::Configure on entity: " << _entity
+        << std::endl;
+}
+
+//////////////////////////////////////////////////
+void ScoringPlugin::PreUpdate(const gz::sim::UpdateInfo &/*_info*/,
+                              gz::sim::EntityComponentManager &/*_ecm*/)
+{
+}
+
+//////////////////////////////////////////////////
+void ScoringPlugin::Update(const gz::sim::UpdateInfo &/*_info*/,
+                           gz::sim::EntityComponentManager &/*_ecm*/)
+{
+}
+
+//////////////////////////////////////////////////
+void ScoringPlugin::PostUpdate(const gz::sim::UpdateInfo &/*_info*/,
+                               const gz::sim::EntityComponentManager &/*_ecm*/) 
+{
+}
+
+//////////////////////////////////////////////////
+void ScoringPlugin::Reset(const gz::sim::UpdateInfo &/*_info*/,
+                          gz::sim::EntityComponentManager &/*_ecm*/)
+{
+  gzdbg << "aic_gazebo::ScoringPlugin::Reset" << std::endl;
+}
+}  // namespace aic_gazebo
