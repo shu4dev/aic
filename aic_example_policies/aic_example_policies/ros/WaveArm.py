0aca45cb0f8fe4269214f2d7cf40b0904d507a29
Morgan Quigley <morganquigley@intrinsic.ai>
2026-02-09 17:03:11 -0500

    Rename policy class to just 'Policy' (#241)

--- aic_example_policies/aic_example_policies/ros/WaveArm.py
+++ aic_example_policies/aic_example_policies/ros/WaveArm.py
@@ -17,8 +17,8 @@
 
 import time
 
-from aic_model.policy_ros import (
-    PolicyRos,
+from aic_model.policy import (
+    Policy,
     GetObservationCallback,
     SetPoseTargetCallback,
     SendFeedbackCallback,
@@ -30,7 +30,7 @@ from rclpy.duration import Duration
 from typing import Callable
 
 
-class WaveArm(PolicyRos):
+class WaveArm(Policy):
     def __init__(self, parent_node):
         super().__init__(parent_node)
         self.get_logger().info("WaveArm.__init__()")
