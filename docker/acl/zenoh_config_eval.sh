a434c3ee12f9f07e0c7478c446dc5e58b19ce701
Morgan Quigley <morganquigley@intrinsic.ai>
2026-02-28 11:43:24 -0500

    simplify and remove unused ACL files

--- docker/acl/zenoh_config_eval.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-HERE="$(dirname "${BASH_SOURCE[0]}")"
-CREDENTIALS="$(readlink -f "$HERE")/credentials.txt"
-ZENOH_CONFIG_OVERRIDE='connect/endpoints=["tcp/127.0.0.1:7448"]'
-ZENOH_CONFIG_OVERRIDE+=';transport/auth/usrpwd/user="eval"'
-ZENOH_CONFIG_OVERRIDE+=';transport/auth/usrpwd/password="CHANGE_IN_PROD"'
-ZENOH_CONFIG_OVERRIDE+=';transport/auth/usrpwd/dictionary_file="'"$CREDENTIALS"'"'
-export ZENOH_CONFIG_OVERRIDE
--- docker/acl/zenoh_config_model.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-HERE="$(dirname "${BASH_SOURCE[0]}")"
-CREDENTIALS="$(readlink -f "$HERE")/model-credentials.txt"
-ZENOH_CONFIG_OVERRIDE='transport/auth/usrpwd/user="model"'
-ZENOH_CONFIG_OVERRIDE+=';transport/auth/usrpwd/password="CHANGE_IN_PROD"'
-ZENOH_CONFIG_OVERRIDE+=';transport/auth/usrpwd/dictionary_file="'"$CREDENTIALS"'"'
-export ZENOH_CONFIG_OVERRIDE
