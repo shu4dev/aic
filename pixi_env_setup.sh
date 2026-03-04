f0ef2271f3d09e57a97122fa951a40053b8ec453
Morgan Quigley <morganquigley@intrinsic.ai>
2026-03-03 19:19:45 -0500

    Disable zenoh shared memory in the pixi environment setup script (#399)

--- pixi_env_setup.sh
+++ pixi_env_setup.sh
@@ -2,4 +2,4 @@
 set -e
 
 export RMW_IMPLEMENTATION="${RMW_IMPLEMENTATION:-rmw_zenoh_cpp}"
-export ZENOH_CONFIG_OVERRIDE="${ZENOH_CONFIG_OVERRIDE:-transport/shared_memory/enabled=true;transport/shared_memory/transport_optimization/pool_size=536870912}"
+export ZENOH_CONFIG_OVERRIDE="transport/shared_memory/enabled=false"
