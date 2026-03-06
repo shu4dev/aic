519d2caf56b9d4d9e46d36866d11ee73e5412550
Morgan Quigley <morganquigley@intrinsic.ai>
2026-03-06 12:46:19 -0500

    Always set RMW_IMPLEMENTATION in pixi_env_setup.sh (#416)

--- pixi_env_setup.sh
+++ pixi_env_setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 set -e
 
-export RMW_IMPLEMENTATION="${RMW_IMPLEMENTATION:-rmw_zenoh_cpp}"
+export RMW_IMPLEMENTATION=rmw_zenoh_cpp
 export ZENOH_CONFIG_OVERRIDE="transport/shared_memory/enabled=false"
