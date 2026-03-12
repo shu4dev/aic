5daeb651f95813678882ad57b739b6505920de9e
Brian Shu <110252035+shu4dev@users.noreply.github.com>
2026-03-11 22:09:30 -1000

    Upgrade JAX with CUDA support in setup script

--- pixi_env_setup.sh
+++ pixi_env_setup.sh
@@ -3,3 +3,4 @@ set -e
 
 export RMW_IMPLEMENTATION=rmw_zenoh_cpp
 export ZENOH_CONFIG_OVERRIDE="transport/shared_memory/enabled=false"
+pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
