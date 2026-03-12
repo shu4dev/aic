99a4f26fea18af7da42aa29618e10f49a51c7bfa
Brian Shu <110252035+shu4dev@users.noreply.github.com>
2026-03-11 23:08:56 -1000

    Fix pip install command for JAX in setup script

--- pixi_env_setup.sh
+++ pixi_env_setup.sh
@@ -3,4 +3,5 @@ set -e
 
 export RMW_IMPLEMENTATION=rmw_zenoh_cpp
 export ZENOH_CONFIG_OVERRIDE="transport/shared_memory/enabled=false"
-pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+"$CONDA_PREFIX/bin/pip" install --upgrade "jax[cuda12_pip]" \
+  -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
